dbd64df373c193309aa9878d834fbd858e51f599
tcwong76 <extreme4dev@gmail.com>
2024-08-21 16:43:52 +0800

    modify cycling-story component

--- src/app/cycling-story/cycling-story.component.ts
+++ src/app/cycling-story/cycling-story.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-cycling-story',
   templateUrl: './cycling-story.component.html',
-  styleUrls: ['./cycling-story.component.css']
+  styleUrls: ['./cycling-story.component.css'],
 })
-export class CyclingStoryComponent {
-
-}
+export class CyclingStoryComponent {}
